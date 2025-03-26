82bc9cf7c7da6fa0aa059a770a96b8791fbd3ac3
Philip-11 <magsaysayphil@gmail.com>
2025-03-26 09:51:35 +0800

    Added css design

--- index.php
+++ index.php
@@ -10,32 +10,27 @@ $posts = getAllPosts($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CMS</title>
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>css/index.css">
 </head>
 <body>
     <?php require 'templates/nav.php'?>
-    <h2>
-        <?php if (isset($_SESSION['logged_in'])) 
-            {
-                if (isset($_SESSION['admin']))
-                {
-                    echo "Hello " . "Admin " . $_SESSION['username'] . "!";
-                }
-                else {
-                    echo "Hello " . $_SESSION['username'] . "!"; 
-                }
-            }
-        ?>
-    </h2>
-    <hr />
+
+    
     <?php foreach ($posts as $row): ?>
-    <h2> <?php echo htmlEscape($row['title']); ?> </h2>
-    <div> <?php echo htmlEscape($row['created_at']); ?> </div>
-    <p> <?php echo htmlEscape($row['description']); ?> </p>
-    <p>
-        <a href="pages/view-post.php?post_id=<?php echo (int) $row['id']?>" 
-        >Read more...</a>
-    </p>
-    <hr />
+    <div class="post">
+        <div class="post-head">
+            <h2> <?php echo htmlEscape($row['title']); ?> </h2>
+        </div>
+        
+        <div class="post-date"> <?php echo htmlEscape($row['created_at']); ?> </div>
+        <p class="post-description"> <?php echo htmlEscape($row['description']); ?> </p>
+        <p>
+            <a class="post-button" href="pages/view-post.php?post_id=<?php echo (int) $row['id']?>" 
+            >Read more...</a>
+        </p>
+    </div>
+    
+   
     <?php endforeach ?>
 </body>
 </html>
--- templates/nav.php
+++ templates/nav.php
@@ -1,20 +1,36 @@
-<nav>   
-        <a href="<?php echo BASE_URL; ?>index.php"><h1>CMS</h1></a>
-        <a href="<?php echo BASE_URL; ?>login.php"><?php if (!isset($_SESSION['logged_in'])) echo "Login"?></a>
-        <a href="<?php echo BASE_URL; ?>register.php"><?php if (!isset($_SESSION['logged_in'])) echo "Register"?></a>
-        <a href="<?php if (isset($_SESSION['admin'])) echo BASE_URL . "pages/admin.php"; else echo BASE_URL . "pages/view-users-post.php";?>">
+<nav class="head-nav">   
+        <div class="nav-left">
+                <a href="<?php echo BASE_URL; ?>index.php">CMS</a>
                 <?php if (isset($_SESSION['logged_in'])) 
+                {
+                        if (isset($_SESSION['admin']))
                         {
-                                if (isset($_SESSION['admin']))
-                                {
-                                        echo "Admin Dashboard";
-                                } else {
-                                        echo "View your posts";
-                                }
+                        echo "Hello " . "Admin " . $_SESSION['username'] . "!";
                         }
-                ?>
-        </a>
-        <a href="<?php echo BASE_URL; ?>pages/add-post.php"><?php if (isset($_SESSION['logged_in'])) echo "Add Post"; ?></a>
-        <a href="<?php echo BASE_URL; ?>logout.php"><?php if (isset($_SESSION['logged_in'])) echo "Logout"; ?></a>
+                        else {
+                        echo "Hello " . $_SESSION['username'] . "!"; 
+                        }
+                }
+        ?>
+        </div>
+        <div class="nav-right">
+                <a href="<?php echo BASE_URL; ?>login.php"><?php if (!isset($_SESSION['logged_in'])) echo "Login"?></a>
+                <a href="<?php echo BASE_URL; ?>register.php"><?php if (!isset($_SESSION['logged_in'])) echo "Register"?></a>
+                <a href="<?php if (isset($_SESSION['admin'])) echo BASE_URL . "pages/admin.php"; else echo BASE_URL . "pages/view-users-post.php";?>">
+                        <?php if (isset($_SESSION['logged_in'])) 
+                                {
+                                        if (isset($_SESSION['admin']))
+                                        {
+                                                echo "Admin Dashboard";
+                                        } else {
+                                                echo "View your posts";
+                                        }
+                                }
+                        ?>
+                </a>
+                <a href="<?php echo BASE_URL; ?>pages/add-post.php"><?php if (isset($_SESSION['logged_in'])) echo "Add Post"; ?></a>
+                <a href="<?php echo BASE_URL; ?>logout.php"><?php if (isset($_SESSION['logged_in'])) echo "Logout"; ?></a>
+        </div>
+        
 </nav>
 
